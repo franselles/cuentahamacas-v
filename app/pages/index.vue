9d82fefee60ec2de22224886aca29eb3c29a730e
franselles <fran.selles@gmail.com>
2026-04-08 21:53:31 +0200

    road_to_hamacas_02

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -72,8 +72,9 @@ const handleLogin = async () => {
         </UCard>
 
         <p class="text-center mt-8 text-xs text-gray-400">
-            v0.0.1 - Control stocks hamacas y sombrillas
+            v0.0.2 - Control stocks hamacas y sombrillas
         </p>
+        <p class="text-center mt-8 text-xs text-gray-400">en desarrollo</p>
         <p class="text-center mt-8 text-xs text-gray-400">R.A. BENIDORM S.L.</p>
     </UContainer>
 </template>
